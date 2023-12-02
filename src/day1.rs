fd569cd0735abaf8d7139eb1cc6c98349fd18219
Alexey Filippov <afilippov@acm.org>
2023-12-02 00:38:50 +0000

    Day 1 cleaned up

--- src/day1.rs
+++ src/day1.rs
@@ -1,19 +1,20 @@
 use std::ops::Index;
 
+const DIGITS: &'static [&'static str] = &[
+    "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+];
+const RADIX: u32 = DIGITS.len() as u32;
+
 #[aoc(day1, part1)]
 pub fn solve_part1(input: &str) -> u32 {
     input
         .lines()
         .map(|s| {
-            let digits: Vec<u32> = s.chars().filter_map(|c| c.to_digit(10)).collect();
-            10 * digits.first().unwrap() + digits.last().unwrap()
+            let digits: Vec<u32> = s.chars().filter_map(|c| c.to_digit(RADIX)).collect();
+            RADIX * digits.first().unwrap() + digits.last().unwrap()
         })
         .sum()
 }
-
-const DIGITS: &'static [&'static str] = &[
-    "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
-];
 
 #[aoc(day1, part2)]
 pub fn solve_part2(input: &str) -> u32 {
@@ -21,7 +22,7 @@ pub fn solve_part2(input: &str) -> u32 {
         .lines()
         .map(|s| {
             let mut digits: Vec<(usize, u32)> = s.chars().enumerate()
-                .filter_map(|(i, c)| c.to_digit(10).map(|d| (i, d)))
+                .filter_map(|(i, c)| c.to_digit(RADIX).map(|d| (i, d)))
                 .collect();
             let mut words: Vec<(usize, u32)> = DIGITS.iter().enumerate()
                 .flat_map(|(d, w)| [(s.find(w), d), (s.rfind(w), d)])
@@ -31,7 +32,7 @@ pub fn solve_part2(input: &str) -> u32 {
             digits.sort();
             let (_, f) = digits.first().unwrap();
             let (_, l) = digits.last().unwrap();
-            10 * f + l
+            RADIX * f + l
         })
         .sum()
 }
