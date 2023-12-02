d4ad8cefcc838112c6c1bb8bde4fd73d2fd7141c
Alexey Filippov <afilippov@acm.org>
2023-12-02 00:22:28 +0000

    Day 1 - slightly less awful

--- src/day1.rs
+++ src/day1.rs
@@ -1,16 +1,12 @@
 use std::ops::Index;
 
-fn digit(c: &char) -> u32 {
-    c.to_digit(10).unwrap()
-}
-
 #[aoc(day1, part1)]
 pub fn solve_part1(input: &str) -> u32 {
     input
         .lines()
         .map(|s| {
-            let digits: Vec<char> = s.chars().filter(|c| c.is_digit(10)).collect();
-            10 * digit(digits.first().unwrap()) + digit(digits.last().unwrap())
+            let digits: Vec<u32> = s.chars().filter_map(|c| c.to_digit(10)).collect();
+            10 * digits.first().unwrap() + digits.last().unwrap()
         })
         .sum()
 }
@@ -25,15 +21,12 @@ pub fn solve_part2(input: &str) -> u32 {
         .lines()
         .map(|s| {
             let mut digits: Vec<(usize, u32)> = s.chars().enumerate()
-                .filter(|(_, c)| c.is_digit(10))
-                .map(|(i, c)| (i, c.to_digit(10).unwrap()))
+                .filter_map(|(i, c)| c.to_digit(10).map(|d| (i, d)))
                 .collect();
-            let mut words: Vec<(usize, u32)> = DIGITS.iter().enumerate().flat_map(|(d, w)| {
-                let l = s.find(w);
-                let r = s.rfind(w);
-                let digit = d as u32;
-                [(l, digit), (r, digit)]
-            }).filter_map(|(p, d)| p.map(|x| (x, d))).collect();
+            let mut words: Vec<(usize, u32)> = DIGITS.iter().enumerate()
+                .flat_map(|(d, w)| [(s.find(w), d), (s.rfind(w), d)])
+                .filter_map(|(p, d)| p.map(|x| (x, d as u32)))
+                .collect();
             digits.append(&mut words);
             digits.sort();
             let (_, f) = digits.first().unwrap();
