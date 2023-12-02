7eb018880a3ad9edb2c01f4d60024e2b93961f55
Alexey Filippov <afilippov@acm.org>
2023-12-02 00:05:04 +0000

    Day 1 task 2

--- src/day1.rs
+++ src/day1.rs
@@ -1,5 +1,7 @@
+use std::ops::Index;
+
 fn digit(c: &char) -> u32 {
-    c.unwrap().to_digit(10).unwrap()
+    c.to_digit(10).unwrap()
 }
 
 #[aoc(day1, part1)]
@@ -13,10 +15,30 @@ pub fn solve_part1(input: &str) -> u32 {
         .sum()
 }
 
+const DIGITS: &'static [&'static str] = &[
+    "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+];
+
 #[aoc(day1, part2)]
 pub fn solve_part2(input: &str) -> u32 {
     input
         .lines()
-        .map(|s| { 1 })
+        .map(|s| {
+            let mut digits: Vec<(usize, u32)> = s.chars().enumerate()
+                .filter(|(_, c)| c.is_digit(10))
+                .map(|(i, c)| (i, c.to_digit(10).unwrap()))
+                .collect();
+            let mut words: Vec<(usize, u32)> = DIGITS.iter().enumerate().flat_map(|(d, w)| {
+                let l = s.find(w);
+                let r = s.rfind(w);
+                let digit = d as u32;
+                [(l, digit), (r, digit)]
+            }).filter_map(|(p, d)| p.map(|x| (x, d))).collect();
+            digits.append(&mut words);
+            digits.sort();
+            let (_, f) = digits.first().unwrap();
+            let (_, l) = digits.last().unwrap();
+            10 * f + l
+        })
         .sum()
 }
