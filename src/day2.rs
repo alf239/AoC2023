1c4f8af562a1a777d5e4d1d4197dbe51a2e7d1ac
Alexey Filippov <afilippov@acm.org>
2023-12-02 00:07:53 +0000

    Day 2 boilerplate

---
+++ src/day2.rs
@@ -0,0 +1,19 @@
+#[aoc(day2, part1)]
+pub fn solve_part1(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|s| {
+            1
+        })
+        .sum()
+}
+
+#[aoc(day2, part2)]
+pub fn solve_part2(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|s| {
+            2
+        })
+        .sum()
+}
