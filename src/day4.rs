f622df21e513ee17f675f777e1a06deec309b87d
Alexey Filippov <afilippov@acm.org>
2023-12-03 12:07:29 +0000

    Day 4 boilerplate

---
+++ src/day4.rs
@@ -0,0 +1,9 @@
+#[aoc(day4, part1)]
+pub fn solve_part1(input: &str) -> u32 {
+    1
+}
+
+#[aoc(day4, part2)]
+pub fn solve_part2(input: &str) -> u32 {
+    2
+}
--- src/lib.rs
+++ src/lib.rs
@@ -7,5 +7,6 @@ extern crate aoc_parse;
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 aoc_lib! { year = 2023 }
