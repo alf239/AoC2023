1c68ea234d93c2a2364e6f68a77395f7a3b251c4
Alexey Filippov <afilippov@acm.org>
2023-12-03 10:19:14 +0000

    More refactoring + fmt

--- src/day3.rs
+++ src/day3.rs
@@ -4,7 +4,7 @@ const RADIX: u32 = 10;
 type Coords = (i32, i32);
 
 #[aoc_generator(day3)]
-pub fn input_generator(input: &str) -> (HashMap<Coords, char>,  Vec<(u32, Coords)>) {
+pub fn input_generator(input: &str) -> (HashMap<Coords, char>, Vec<(u32, Coords)>) {
     let mut symbols: HashMap<Coords, char> = HashMap::new();
     let mut numbers: Vec<(u32, Coords)> = Vec::new();
 
@@ -34,25 +34,33 @@ pub fn input_generator(input: &str) -> (HashMap<Coords, char>,  Vec<(u32, Coords
     (symbols, numbers)
 }
 
+fn has_neighbour(number: u32, pos: Coords, symbols: &HashMap<Coords, char>) -> bool {
+    let (y, x) = pos;
+    let len = number.to_string().len() as i32;
+    let lt = x - 1;
+    let rt = x + len;
+    let vert = (lt..=rt)
+        .flat_map(|xn| [(y - 1, xn), (y + 1, xn)])
+        .any(|pos| symbols.contains_key(&pos));
+    let sides = symbols.contains_key(&(y, lt)) || symbols.contains_key(&(y, rt));
+    vert || sides
+}
 
 #[aoc(day3, part1)]
-pub fn solve_part1((symbols, numbers): &(HashMap<Coords, char>,  Vec<(u32, Coords)>)) -> u32 {
-    numbers.iter().filter_map(|(number, (y, x))| {
-        let len= number.to_string().len() as i32;
-        let lt = x - 1;
-        let rt = x + len;
-        let vert = (lt..=rt).flat_map(|xn| [(y - 1, xn), (y + 1, xn)]).any(|pos| symbols.contains_key(&pos));
-        let sides = symbols.contains_key(&(*y, lt)) || symbols.contains_key(&(*y, rt));
-        if vert || sides {
-            Some(number)
-        } else {
-            None
-        }
-    }).sum()
+pub fn solve_part1((symbols, numbers): &(HashMap<Coords, char>, Vec<(u32, Coords)>)) -> u32 {
+    numbers
+        .iter()
+        .filter_map(|(number, pos)| {
+            if has_neighbour(*number, *pos, symbols) {
+                Some(number)
+            } else {
+                None
+            }
+        })
+        .sum()
 }
 
 #[aoc(day3, part2)]
-pub fn solve_part2((symbols, numbers): &(HashMap<Coords, char>,  Vec<(u32, Coords)>)) -> u32 {
+pub fn solve_part2((symbols, numbers): &(HashMap<Coords, char>, Vec<(u32, Coords)>)) -> u32 {
     2
 }
-
