15d4459c5f7f828d76ce33c16630d55e4762d84d
Alexey Filippov <afilippov@acm.org>
2023-12-09 23:08:41 +0000

    Day 10 boilerplate

---
+++ src/day10.rs
@@ -0,0 +1,36 @@
+use aoc_parse::{parser, prelude::*};
+
+type Task = String;
+
+#[aoc_generator(day9)]
+pub fn input_generator(input: &str) -> Task {
+    // let p = parser!(seqs:lines(repeat_sep(i32, " ")) => Task { seqs });
+    // p.parse(input).unwrap()
+    input.to_string()
+}
+
+#[aoc(day10, part1)]
+fn solve_part1(input: &Task) -> i32 {
+    1
+}
+
+#[aoc(day10, part2)]
+fn solve_part2(input: &Task) -> i32 {
+    2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example1() {
+        let input = r#""#
+            .trim();
+        let parsed = input_generator(input);
+        let result1 = solve_part1(&parsed);
+        assert_eq!(result1, 1);
+        let result2 = solve_part2(&parsed);
+        assert_eq!(result2, 2);
+    }
+}
