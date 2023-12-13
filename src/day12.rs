45b260019340fb17de6fdce340d7613d9b82dd27
Alexey Filippov <afilippov@acm.org>
2023-12-13 14:31:08 +0000

    Day 12 part 2

--- src/day12.rs
+++ src/day12.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashSet, iter::repeat};
+
 use aoc_parse::{parser, prelude::*};
 
 type Task = Vec<(String, Vec<usize>)>;
@@ -15,39 +17,74 @@ fn arr(s: &str, jp: &[usize]) -> u64 {
     arrangements(&vec, jp)
 }
 
-fn arrangements(s: &[char], jp: &[usize]) -> u64 {
-    match (s, jp) {
-        ([], []) => 1,
-        ([], _) => 0,
-        (_, []) if s.iter().all(|&c| ".?".contains(c)) => 1,
-        (_, []) => 0,
-        (['.', rest @ ..], _) => arrangements(rest, jp),
-        (['#', rest @ ..], [nr, tail @ ..])
-            if s.len() >= *nr
-                && rest[..nr - 1].iter().all(|&c| "?#".contains(c))
-                && (s.len() == *nr || ".?".contains(rest[nr - 1])) =>
-        {
-            if *nr >= rest.len() {
-                arrangements(&[], tail)
-            } else {
-                arrangements(&rest[*nr..], tail)
+fn arr2(s: &str, jp: &[usize]) -> u64 {
+    let s = repeat(s).take(5).collect::<Vec<_>>().join("?");
+    let vec: Vec<char> = s.chars().collect();
+    let jp1: Vec<usize> = jp.iter().cycle().take(jp.len() * 5).cloned().collect();
+    arrangements(&vec, &jp1)
+}
+
+fn arrangements(s1: &[char], jp: &[usize]) -> u64 {
+    let mut s2 = Vec::from(s1);
+    s2.insert(0, '.');
+    let s: Vec<char> = s2.iter().cloned().collect(); 
+    let empty = HashSet::from([0]);
+    let mut ls: Vec<HashSet<usize>> = (0..s.len() + 1).map(|_| empty.clone()).collect();
+    let mut cur = empty.clone();
+    for (j, &c) in s.iter().enumerate() {
+        match c {
+            '#' => {
+                cur = cur.iter().map(|x| x + 1).collect();
             }
-        }
-        (['?', rest @ ..], [nr, tail @ ..])
-            if s.len() >= *nr
-                && rest[..nr - 1].iter().all(|&c| "?#".contains(c))
-                && (s.len() == *nr || ".?".contains(rest[nr - 1])) =>
-        {
-            let if_skip = arrangements(rest, jp);
-            if *nr >= rest.len() {
-                arrangements(&[], tail) + if_skip
-            } else {
-                arrangements(&rest[*nr..], tail) + if_skip
+            '.' => {
+                cur = empty.clone();
             }
+            '?' => {
+                cur = cur.iter().map(|x| x + 1).collect();
+                cur.insert(0);
+            }
+            _ => panic!("Unknown character {}", c),
         }
-        (['?', rest @ ..], _) => arrangements(rest, jp),
-        _ => 0,
+        ls[j + 1] = cur.clone();
     }
+    let mut dp = vec![vec![0u64; s.len() + 1]; jp.len() + 1];
+    dp[0][0] = 1;
+    for i in 1..dp.len() {
+        dp[i][0] = 0;
+    }
+    let mut saw_hash = false;
+    for j in 1..dp[0].len() {
+        saw_hash |= s[j - 1] == '#';
+        dp[0][j] = if saw_hash { 0 } else { 1 };
+    }
+    for i in 1..dp.len() {
+        for j in 1..dp[i].len() {
+            let c = s[j - 1];
+            match c {
+                '.' => dp[i][j] = dp[i][j - 1],
+                '#' => {
+                    let n = jp[i - 1];
+                    let consume_nr = if ls[j].contains(&n) {
+                        dp[i - 1][j - n - 1]
+                    } else {
+                        0
+                    };
+                    dp[i][j] = consume_nr;
+                }
+                '?' => {
+                    let n = jp[i - 1];
+                    let consume_nr = if ls[j].contains(&n) {
+                        dp[i - 1][j - n - 1]
+                    } else {
+                        0
+                    };
+                    dp[i][j] = consume_nr + dp[i][j - 1];
+                }
+                _ => panic!("Unknown character {}", c),
+            }
+        }
+    }
+    dp[jp.len()][s.len()]
 }
 
 #[aoc(day12, part1)]
@@ -57,7 +94,7 @@ fn solve_part1(input: &Task) -> u64 {
 
 #[aoc(day12, part2)]
 fn solve_part2(input: &Task) -> u64 {
-    2
+    input.iter().map(|(s, jp)| arr2(s, jp)).sum()
 }
 
 #[cfg(test)]
@@ -75,6 +112,16 @@ mod tests {
     }
 
     #[test]
+    fn known_arrangements2() {
+        assert_eq!(arr2("???.###", &vec![1, 1, 3]), 1);
+        assert_eq!(arr2(".??..??...?##.", &vec![1, 1, 3]), 16384);
+        assert_eq!(arr2("?#?#?#?#?#?#?#?", &vec![1, 3, 1, 6]), 1);
+        assert_eq!(arr2("????.#...#...", &vec![4, 1, 1]), 16);
+        assert_eq!(arr2("????.######..#####.", &vec![1, 6, 5]), 2500);
+        assert_eq!(arr2("?###????????", &vec![3, 2, 1]), 506250);
+    }
+
+    #[test]
     fn example1() {
         let input = r#"
 ???.### 1,1,3
@@ -88,6 +135,6 @@ mod tests {
         let result1 = solve_part1(&parsed);
         assert_eq!(result1, 21);
         let result2 = solve_part2(&parsed);
-        assert_eq!(result2, 2);
+        assert_eq!(result2, 525152);
     }
 }
