f20ff29fdaa99c6b60d8982be0a897afa248da5d
Alexey Filippov <afilippov@acm.org>
2023-12-14 22:55:17 +0000

    Day 15 boilerplate

---
+++ src/day15.rs
@@ -0,0 +1,39 @@
+use std::{collections::HashMap, mem::swap};
+
+use aoc_parse::{parser, prelude::*};
+
+type Task = Vec<Vec<usize>>;
+
+#[aoc_generator(day15)]
+pub fn input_generator(input: &str) -> Task {
+    let p = parser!(
+        lines(char_of(".#O")+)
+    );
+    p.parse(input).unwrap()
+}
+
+#[aoc(day15, part1)]
+fn solve_part1(input: &Task) -> usize {
+    1
+}
+
+#[aoc(day15, part2)]
+fn solve_part2(input: &Task) -> usize {
+    2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example1() {
+        let input = r#""#
+            .trim();
+        let parsed = input_generator(input);
+        let result1 = solve_part1(&parsed);
+        assert_eq!(result1, 1);
+        let result2 = solve_part2(&parsed);
+        assert_eq!(result2, 2);
+    }
+}
