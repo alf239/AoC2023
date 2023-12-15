95bee18a24ebdfe20c54dbb5f22f181fc82c7f1b
Alexey Filippov <afilippov@acm.org>
2023-12-15 17:25:46 +0000

    Tidy up

--- src/day15.rs
+++ src/day15.rs
@@ -1,25 +1,70 @@
-use std::{collections::HashMap, mem::swap};
-
 use aoc_parse::{parser, prelude::*};
 
-type Task = Vec<Vec<usize>>;
-
-#[aoc_generator(day15)]
-pub fn input_generator(input: &str) -> Task {
-    let p = parser!(
-        lines(char_of(".#O")+)
-    );
-    p.parse(input).unwrap()
+fn hash(s: &str) -> usize {
+    let mut res: usize = 0;
+    for &c in s.as_bytes() {
+        res += c as usize;
+        res *= 17;
+        res = res % 256;
+    }
+    res
 }
 
 #[aoc(day15, part1)]
-fn solve_part1(input: &Task) -> usize {
-    1
+fn solve_part1(input: &str) -> usize {
+    input.trim().split(",").map(|s| hash(s)).sum()
+}
+
+#[derive(PartialEq, Eq, Debug)]
+enum Cmd {
+    Rm(String),
+    Put(String, usize),
+}
+
+fn parse(s: &str) -> Cmd {
+    use Cmd::*;
+    let p = parser!({
+        name:string(lower+) "=" val:usize => Put(name, val),
+        name:string(lower+) "-" => Rm(name),
+    });
+    p.parse(s).unwrap()
 }
 
 #[aoc(day15, part2)]
-fn solve_part2(input: &Task) -> usize {
-    2
+fn solve_part2(input: &str) -> usize {
+    let program: Vec<Cmd> = input.trim().split(",").map(|s| parse(s)).collect();
+    let mut hm: Vec<Vec<(String, usize)>> = (0..256).map(|_| Vec::new()).collect();
+    for cmd in program {
+        match cmd {
+            Cmd::Rm(name) => {
+                let h = hash(&name);
+                let i = hm[h].iter().position(|(s, _)| *s == name);
+                match i {
+                    Some(ix) => {
+                        hm[h].remove(ix);
+                    }
+                    None => {}
+                }
+            }
+            Cmd::Put(name, value) => {
+                let h = hash(&name);
+                let i = hm[h].iter().position(|(s, _)| *s == name);
+                match i {
+                    Some(ix) => hm[h][ix] = (name, value),
+                    None => hm[h].push((name, value)),
+                }
+            }
+        }
+    }
+    hm.iter()
+        .enumerate()
+        .flat_map(|(i, bucket)| {
+            bucket
+                .iter()
+                .enumerate()
+                .map(move |(j, (_, v))| (i + 1) * (j + 1) * v)
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -27,13 +72,16 @@ mod tests {
     use super::*;
 
     #[test]
+    fn example_hash() {
+        assert_eq!(hash("HASH"), 52);
+    }
+
+    #[test]
     fn example1() {
-        let input = r#""#
-            .trim();
-        let parsed = input_generator(input);
-        let result1 = solve_part1(&parsed);
-        assert_eq!(result1, 1);
-        let result2 = solve_part2(&parsed);
-        assert_eq!(result2, 2);
+        let input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7\n\n";
+        let result1 = solve_part1(&input);
+        assert_eq!(result1, 1320);
+        let result2 = solve_part2(&input);
+        assert_eq!(result2, 145);
     }
 }
