07e914ada84ab8deecf361fb31fd41ea2cda6d87
Alexey Filippov <afilippov@acm.org>
2023-12-16 10:50:52 +0000

    Day 16 task 1

---
+++ src/day16.rs
@@ -0,0 +1,127 @@
+use std::{
+    collections::{HashMap, HashSet, VecDeque},
+    mem::swap,
+};
+
+use aoc_parse::{parser, prelude::*};
+
+type Coords = (i32, i32);
+
+#[derive(Clone, Copy)]
+pub enum Tile {
+    Hor,
+    Ver,
+    Fwd,
+    Bck,
+}
+
+pub struct Task {
+    h: i32,
+    w: i32,
+    m: HashMap<Coords, Tile>,
+}
+
+#[aoc_generator(day16)]
+pub fn input_generator(input: &str) -> Task {
+    use Tile::*;
+    let p = parser!(
+        lines({
+            "." => None,
+            "/" => Some(Fwd),
+            "\\" => Some(Bck),
+            "|" => Some(Ver),
+            "-" => Some(Hor),
+        }+)
+    );
+    let tiles = p.parse(input).unwrap();
+    let m: HashMap<Coords, Tile> = tiles
+        .iter()
+        .enumerate()
+        .flat_map(|(i, row)| {
+            row.iter()
+                .enumerate()
+                .filter_map(move |(j, c)| c.map(|x| ((i as i32, j as i32), x)))
+        })
+        .collect();
+    Task {
+        h: tiles.len() as i32,
+        w: tiles[0].len() as i32,
+        m,
+    }
+}
+
+type Particle = (Coords, Coords);
+
+#[aoc(day16, part1)]
+fn solve_part1(input: &Task) -> usize {
+    let mut energised: HashSet<Coords> = HashSet::new();
+    let mut seen: HashSet<Particle> = HashSet::new();
+    let mut work: VecDeque<Particle> = VecDeque::from([((0, 0), (0, 1))]);
+    while !work.is_empty() {
+        let p @ (pos @ (i, j), (di, dj)) = work.pop_front().unwrap();
+        if i < 0 || j < 0 || i >= input.h || j >= input.w {
+            continue;
+        }
+        if !seen.insert(p) {
+            continue;
+        }
+        energised.insert(pos);
+        match input.m.get(&pos) {
+            None => {
+                work.push_back(((i + di, j + dj), (di, dj)));
+            }
+            Some(Tile::Hor) if di == 0 => {
+                work.push_back(((i + di, j + dj), (di, dj)));
+            }
+            Some(Tile::Ver) if dj == 0 => {
+                work.push_back(((i + di, j + dj), (di, dj)));
+            }
+            Some(Tile::Hor) => {
+                work.push_back(((i, j + 1), (0, 1)));
+                work.push_back(((i, j - 1), (0, -1)));
+            }
+            Some(Tile::Ver) => {
+                work.push_back(((i + 1, j), (1, 0)));
+                work.push_back(((i - 1, j), (-1, 0)));
+            }
+            Some(Tile::Bck) => {
+                work.push_back(((i + dj, j + di), (dj, di)));
+            }
+            Some(Tile::Fwd) => {
+                work.push_back(((i - dj, j - di), (-dj, -di)));
+            }
+        }
+    }
+    energised.len()
+}
+
+#[aoc(day16, part2)]
+fn solve_part2(input: &Task) -> usize {
+    2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example1() {
+        let input = r#"
+.|...\....
+|.-.\.....
+.....|-...
+........|.
+..........
+.........\
+..../.\\..
+.-.-/..|..
+.|....-|.\
+..//.|...."#
+            .trim();
+        let parsed = input_generator(input);
+        let result1 = solve_part1(&parsed);
+        assert_eq!(result1, 46);
+        let result2 = solve_part2(&parsed);
+        assert_eq!(result2, 2);
+    }
+}
